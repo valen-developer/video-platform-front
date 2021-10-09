75d3e90a817cd27aca659918e650e3603f3ccab5
Valentin Valverde <valenreta.developer@gmail.com>
2021-10-09 20:01:48 +0200

    add gesture detector public section

--- src/app/presentation/public/public.component.ts
+++ src/app/presentation/public/public.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+import * as hammerjs from 'hammerjs';
 
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
-  styleUrls: ['./public.component.scss']
+  styleUrls: ['./public.component.scss'],
 })
-export class PublicComponent implements OnInit {
+export class PublicComponent implements OnInit, AfterViewInit {
+  @ViewChild('public') public publicDivElement: ElementRef<HTMLDivElement>;
 
-  constructor() { }
+  public isOpenNavbar = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.manageGesture();
   }
 
+  private manageGesture() {
+    const mg = new hammerjs.Manager(this.publicDivElement.nativeElement, {
+      recognizers: [[Hammer.Swipe, { direction: Hammer.DIRECTION_HORIZONTAL }]],
+    });
+
+    mg.on('swiperight', () => this.openNavbar());
+    mg.on('swipeleft', () => this.hideNavbar());
+  }
+
+  private openNavbar(): void {
+    console.log('open');
+    this.isOpenNavbar = true;
+  }
+  private hideNavbar(): void {
+    this.isOpenNavbar = false;
+  }
 }
