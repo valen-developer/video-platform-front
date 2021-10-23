bda4ebe7edd86aa8b09bf3695a1a5a0700787ea2
Valentin Valverde <valenreta.developer@gmail.com>
2021-10-23 21:15:01 +0200

    creating checkbox component

---
+++ src/app/presentation/shared/components/checkbox/checkbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss'],
+})
+export class CheckboxComponent implements OnInit {
+  @Input() public formControl: FormControl;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/presentation/shared/shared.module.ts
+++ src/app/presentation/shared/shared.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Components
 import { GeneralLoadingComponent } from './components/general-loading/general-loading.component';
-import { DropZoneDirective } from './directives/drop-zone.directive';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+
+// Directives
 import { OpenNavbarSwipperDirective } from './directives/open-navbar-swipper.directive';
+import { DropZoneDirective } from './directives/drop-zone.directive';
 
 @NgModule({
   declarations: [
     GeneralLoadingComponent,
     DropZoneDirective,
     OpenNavbarSwipperDirective,
+    CheckboxComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [
     GeneralLoadingComponent,
+    CheckboxComponent,
     DropZoneDirective,
     OpenNavbarSwipperDirective,
   ],
